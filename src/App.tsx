b727bda9fb2a61efb3ef07ce1b13b2c3da8db297
Omar Salem <omar123789ali@gmail.com>
2024-03-04 08:46:33 +0200

    Layout done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
+// min 1:30:00 https://www.youtube.com/watch?v=6a3Dz8gwjdg&t=2409s&ab_channel=JavaScriptMastery
 import {
   ErrorComponent,
   ThemedLayoutV2,
@@ -20,18 +20,9 @@ import routerBindings, {
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import {
-  BlogPostCreate,
-  BlogPostEdit,
-  BlogPostList,
-  BlogPostShow,
-} from "./pages/blog-posts";
-import {
-  CategoryCreate,
-  CategoryEdit,
-  CategoryList,
-  CategoryShow,
-} from "./pages/categories";
+
+import Layout from "./components/layout";
+import { resources } from "./config/resources";
 
 
 
@@ -48,41 +39,23 @@ function App() {
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
-                resources={[
-                  {
-                    name: "blog_posts",
-                    list: "/blog-posts",
-                    create: "/blog-posts/create",
-                    edit: "/blog-posts/edit/:id",
-                    show: "/blog-posts/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                  {
-                    name: "categories",
-                    list: "/categories",
-                    create: "/categories/create",
-                    edit: "/categories/edit/:id",
-                    show: "/categories/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                ]}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                  useNewQueryKeys: true,
-                  projectId: "7Jgb4s-CIGlKo-skla44",
-                  liveMode: "auto",
-                }}
+                resources={resources}
               >
                 <Routes>
-                  <Route index element={<Home />} />
                   <Route path="/forget-password" element={<ForgotPassword />} />
                   <Route path="/login" element={<Login />} />
                   <Route path="/register" element={<Register />} />
+                  <Route
+                  element ={<Authenticated
+                    key="authenticated-layout"
+                    fallback={<CatchAllNavigate to="/login" />}>
+                      <Layout>
+                        <Outlet />
+                      </Layout>
+                    </Authenticated>}
+                  >
+                    <Route index element={<Home />} />
+                  </Route>
                 </Routes>
 
                 <RefineKbar />
